ab15e0788c1afaa3e2da0376a4e2440dded74e4b
meoflynn <megan.o'flynn@stfc.ac.uk>
2024-11-20 15:50:53 +0000

    ENH: Update dockerfile and rename script
    Docerfile includes steps for copying and running the script.
    Script renamed to make it clear what dependencies are being installed

---
+++ jupyter-llm-notebook/install-sciml-deps.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo -e "\nUninstalling conflicting libraries...\n"
+pip uninstall -y tensorflow
+
+echo -e "\nInstalling essential Python libraries...\n"
+pip install --no-cache-dir --upgrade flask gdown jupyter matplotlib numpy pandas scipy spacy tqdm
+
+echo -e "\nInstalling PyTorch...\n"
+pip install --no-cache-dir --upgrade torch torchvision torchaudio
+
+echo -e "\nInstalling HuggingFace libraries...\n"
+pip install --no-cache-dir --upgrade huggingface_hub transformers accelerate
+
+echo -e "\nInstalling bitsandbytes...\n"
+pip install --no-cache-dir bitsandbytes -U
+
+echo -e "\nInstalling LLM libraries...\n"
+pip install --no-cache-dir --upgrade faiss-cpu networkx langchain langchain_community langgraph vllm
+
+echo -e "\nDownloading spaCy language model...\n"
+python -m spacy download en_core_web_sm
+
+echo -e "\nInstalling Localtunnel...\n"
+npm install -g localtunnel
+
+echo -e "\nInstalling Ollama...\n"
+OLLAMA_DIR="/home/${NB_USER}/ollama"
+OLLAMA_RELEASE_URL="https://github.com/ollama/ollama/releases/download/v0.4.2/ollama-linux-amd64.tgz"
+mkdir -p "$OLLAMA_DIR"
+wget --quiet --show-progress -O- "$OLLAMA_RELEASE_URL" | tar -xz -C "$OLLAMA_DIR"
