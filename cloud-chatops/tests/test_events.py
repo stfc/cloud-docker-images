1809ba3d33eddd2144f15127f216d5aa1f136bbc
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2024-11-22 13:43:05 +0000

    TEST: Add new unit tests for refactored code

--- cloud-chatops/tests/test_events.py
+++ cloud-chatops/tests/test_events.py
@@ -2,23 +2,46 @@
 
 from unittest.mock import patch
 from events import run_global_reminder, run_personal_reminder
+from pr_dataclass import PRProps
 
 
-@patch("events.PostPRsToSlack")
-def test_run_global_reminder(mock_post_prs_to_slack):
+@patch("events.WebClient")
+@patch("events.get_token")
+@patch("events.get_config")
+@patch("events.FindPRs")
+@patch("events.PRReminder")
+def test_run_global_reminder(mock_pr_reminder, mock_find_prs, mock_get_config, mock_get_token, mock_web_client):
     """Test global reminder event"""
     run_global_reminder("mock_channel")
-    mock_post_prs_to_slack.return_value.run.assert_called_once_with(
+    mock_find_prs.return_value.run.assert_called_once_with(
+        repos=mock_get_config.return_value,
+        sort=(PRProps.CREATED_AT, False)
+    )
+    mock_pr_reminder.assert_called_once_with(mock_web_client.return_value)
+    mock_pr_reminder.return_value.run.assert_called_once_with(
+        prs=mock_find_prs.return_value.run.return_value,
         channel="mock_channel"
     )
+    mock_get_config.assert_called_once_with("repos")
+    mock_get_token.assert_called_once_with("SLACK_BOT_TOKEN")
 
 
+@patch("events.WebClient")
+@patch("events.get_token")
 @patch("events.get_config")
-@patch("events.PostToDMs")
-def test_run_personal_reminder(mock_post_to_dms, mock_config):
+@patch("events.FindPRs")
+@patch("events.PRReminder")
+def test_run_personal_reminder(mock_pr_reminder, mock_find_prs, mock_get_config, mock_get_token, mock_web_client):
     """Test personal reminder event"""
-    mock_config.return_value = {"mock_github_name": "mock_slack_name"}
-    run_personal_reminder()
-    mock_post_to_dms.return_value.run.assert_called_once_with(
-        users=["mock_slack_name"], post_all=False
-    )
+    mock_repos = {"mock_owner": ["mock_repo"]}
+    mock_get_config.side_effect = [mock_repos, {"mock_github": "mock_slack"}]
+    run_personal_reminder(["mock_slack"])
+    mock_find_prs.assert_called_once()
+    mock_find_prs.return_value.run.assert_called_once_with(repos=mock_repos, sort=(PRProps.CREATED_AT, False))
+    mock_get_config.assert_any_call("repos")
+    mock_get_config.assert_any_call("user-map")
+    mock_web_client.assert_called_once_with(token=mock_get_token.return_value)
+    mock_get_token.assert_called_once_with("SLACK_BOT_TOKEN")
+    mock_pr_reminder.assert_called_once_with(mock_web_client.return_value)
+    mock_pr_reminder.return_value.run.assert_called_once_with(prs=mock_find_prs.return_value.run.return_value, channel="mock_slack", filter_by=(PRProps.AUTHOR, "mock_github"), message_no_prs=False)
+
