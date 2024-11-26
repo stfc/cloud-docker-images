70181367555b2a3bc8cb17460bac8abf0f618f63
David Fairbrother <DavidFair@users.noreply.github.com>
2024-11-26 13:48:57 +0000

    Use different base and let resolver resolve
    Use a different base, since we're effectively taking everything from the
    upstream package, and re-installing to a different version when we could
    just use the community image at that point
    
    Also change the pip install to install all packages at once, so it
    considers their requirements, as existing packages aren't considered in
    dep resolution potentially causing problems later

--- jupyter-llm-notebook/install-sciml-deps.sh
+++ jupyter-llm-notebook/install-sciml-deps.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
-echo -e "\nUninstalling conflicting libraries...\n"
-pip uninstall -y tensorflow
+set -eux -o pipefail
 
-echo -e "\nInstalling essential Python libraries...\n"
-pip install --no-cache-dir --upgrade flask gdown jupyter matplotlib numpy pandas scipy spacy tqdm
+# Skip installing torch audio and vision
+# as they're installed here https://github.com/jupyter/docker-stacks/blob/main/images/pytorch-notebook/cuda12/Dockerfile#L16
 
-echo -e "\nInstalling PyTorch...\n"
-pip install --no-cache-dir --upgrade torch torchvision torchaudio
+echo -e "\nInstalling all pip libraries...\n"
+pip install --no-cache-dir --upgrade \
+    flask gdown jupyter matplotlib numpy pandas scipy spacy tqdm \
+    huggingface_hub transformers accelerate datasets \
+    bitsandbytes \
+    faiss-cpu networkx langchain langchain_community langgraph vllm \
+    spacy download
 
-echo -e "\nInstalling HuggingFace libraries...\n"
-pip install --no-cache-dir --upgrade huggingface_hub transformers accelerate datasets
-
-echo -e "\nInstalling bitsandbytes...\n"
-pip install --no-cache-dir bitsandbytes -U
-
-echo -e "\nInstalling LLM libraries...\n"
-pip install --no-cache-dir --upgrade faiss-cpu networkx langchain langchain_community langgraph vllm
-
-echo -e "\nDownloading spaCy language model...\n"
+echo -e "\nDownloading spacy model...\n"
 python -m spacy download en_core_web_sm
 
 echo -e "\nInstalling Localtunnel...\n"
 npm install -g localtunnel
-
-echo -e "\nInstalling Ollama...\n"
-OLLAMA_DIR="/opt/ollama"
-OLLAMA_RELEASE_URL="https://github.com/ollama/ollama/releases/download/v0.4.2/ollama-linux-amd64.tgz"
-wget --quiet -O- "$OLLAMA_RELEASE_URL" | tar -xz -C "$OLLAMA_DIR"
