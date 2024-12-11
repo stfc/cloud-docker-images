19d9cd7d7675da637fbddb3edc4a6ecc9136523e
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2024-12-11 13:56:14 +0000

    MAINT: Move the path finder into a function
    Moving the path finder logic into a function allows us to test it easier

--- cloud-chatops/src/read_data.py
+++ cloud-chatops/src/read_data.py
@@ -7,23 +7,62 @@ import yaml
 from errors import ErrorInConfig
 from data import User
 
-# Production config path
-PATH = "/usr/src/app/cloud_chatops/"
 
-if sys.argv[0].endswith("dev.py"):
-    # Using dev secrets here for local testing as it runs the app
-    # in a separate Slack Workspace than the production app.
-    # This means the slash commands won't be picked up by the production app.
-    try:
-        # Try multiple paths for Linux / Windows differences
-        PATH = f"{os.environ['HOME']}/dev_cloud_chatops/"
-    except KeyError:
+def get_path() -> str:
+    """
+    Determine which path to use for reading secrets.
+    This differs between development and production environments.
+    :return: The path as a string
+    """
+    if sys.argv[0].endswith("dev.py"):
+        # Using dev secrets here for local testing as it runs the app.
+        # in a separate Slack Workspace than the production app.
+        # This means the slash commands won't be picked up by the production app.
         try:
-            PATH = f"{os.environ['HOMEPATH']}\\dev_cloud_chatops\\"
-        except KeyError as exc:
-            raise ErrorInConfig(
-                "Are you trying to run locally? Couldn't find HOME or HOMEPATH in your environment variables."
-            ) from exc
+            # Try multiple paths for Linux / Windows differences
+            return f"{os.environ['HOME']}/dev_cloud_chatops/"
+        except KeyError:
+            try:
+                return f"{os.environ['HOMEPATH']}\\dev_cloud_chatops\\"
+            except KeyError as exc:
+                raise ErrorInConfig(
+                    "Are you trying to run locally? Couldn't find HOME or HOMEPATH in your environment variables."
+                ) from exc
+
+    # Production path
+    return "/usr/src/app/cloud_chatops/"
+
+
+def get_token(secret: str) -> str:
+    """
+    This function reads from the secret's file and returns a specified secret.
+    :param secret: The secret to find
+    :return: A secret as string
+    """
+    path = get_path()
+    with open(path + "secrets/secrets.yml", "r", encoding="utf-8") as secrets:
+        secrets_data = yaml.safe_load(secrets)
+        return secrets_data[secret]
+
+
+def get_config(section: str) -> Union[List | Dict]:
+    """
+    This function returns the specified section from the config file.
+    :param section: The section of the config to retrieve.
+    :return: The data retrieved from the config file.
+    """
+    path = get_path()
+    with open(path + "config/config.yml", "r", encoding="utf-8") as config:
+        config_data = yaml.safe_load(config)
+        match section:
+            case "users":
+                return [User.from_config(user) for user in config_data[section]]
+            case "repos":
+                return config_data[section]
+            case "channel":
+                return config_data[section]
+            case _:
+                raise KeyError(f"No section in config named {section}.")
 
 
 def validate_required_files() -> None:
@@ -42,33 +81,3 @@ def validate_required_files() -> None:
 
     if not get_config("channel"):
         raise ErrorInConfig("Channel parameter in config.yml is not set.")
-
-
-def get_token(secret: str) -> str:
-    """
-    This function reads from the secret's file and returns a specified secret.
-    :param secret: The secret to find
-    :return: A secret as string
-    """
-    with open(PATH + "secrets/secrets.yml", "r", encoding="utf-8") as secrets:
-        secrets_data = yaml.safe_load(secrets)
-        return secrets_data[secret]
-
-
-def get_config(section: str) -> Union[List | Dict]:
-    """
-    This function returns the specified section from the config file.
-    :param section: The section of the config to retrieve.
-    :return: The data retrieved from the config file.
-    """
-    with open(PATH + "config/config.yml", "r", encoding="utf-8") as config:
-        config_data = yaml.safe_load(config)
-        match section:
-            case "users":
-                return [User.from_config(user) for user in config_data[section]]
-            case "repos":
-                return config_data[section]
-            case "channel":
-                return config_data[section]
-            case _:
-                raise KeyError(f"No section in config named {section}.")
