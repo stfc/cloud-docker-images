c2ae4292c1082123be09604221cbac3b323455de
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-01-08 13:35:50 +0000

    MAINT: Removing unused Enums
    This Enum class contains only 2 states which can be replaced with a Bool. This code is also not used so doesn't need to be here

--- cloud-chatops/src/enum_states.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=C0114
-from enum import Enum, auto
-
-
-class PRsFoundState(Enum):
-    """This Enum provides states for whether any PRs were found."""
-
-    PRS_FOUND = auto()
-    NONE_FOUND = auto()
