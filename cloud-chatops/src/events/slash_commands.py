f296171652be4b1e44051978c2e79da3a17dd1e8
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-01-27 15:32:22 +0000

    ENH: Add slash command for finding GitLab reminders
    Gives users the option to find all merge requests from their gitlab projects

--- cloud-chatops/src/events/slash_commands.py
+++ cloud-chatops/src/events/slash_commands.py
@@ -2,6 +2,7 @@ import os
 from helper.data import filter_by
 from helper.read_config import get_config, get_token
 from find_pr_api.github import FindPRs as FindPRsGitHub
+from find_pr_api.gitlab import FindPRs as FindPRsGitLab
 from slack_reminder_api.pr_reminder import send_reminders
 
 
@@ -47,3 +48,36 @@ def slash_find_host(ack, respond):
     ack()
     host_ip = os.environ.get("HOST_IP")
     respond(f"The host IP of this node is: {host_ip}")
+
+
+def slash_mrs(ack, respond, command):
+    """
+    This event sends a message to the user containing open MRs.
+    :param command: The return object from Slack API.
+    :param ack: Slacks acknowledgement command.
+    :param respond: Slacks respond command to respond to the command in chat.
+    """
+    ack()
+    users = get_config("users")
+    if command["user_id"] not in [user.slack_id for user in users]:
+        respond(
+            f"Could not find your Slack ID {command["user_id"]} in the user map. "
+            f"Please contact the service maintainer to fix this."
+        )
+        return
+
+    if command["text"] != "mine" and command["text"] != "all":
+        respond("Please provide the correct argument: 'mine' or 'all'.")
+        return
+
+    user = filter_by(users, "slack_id", command["user_id"])[0]
+
+    respond("Finding the MRs...")
+    prs = FindPRsGitLab().run(projects=get_config("projects"), token=get_token("GITLAB_TOKEN"))
+
+    if command["text"] == "mine":
+        prs = filter_by(obj_list=prs, prop="author", value=user.gitlab_name)
+        send_reminders(user.slack_id, prs, True)
+
+    elif command["text"] == "all":
+        send_reminders(user.slack_id, prs, True)
--- cloud-chatops/tests/test_slash_commands.py
+++ cloud-chatops/tests/test_slash_commands.py
@@ -1,15 +1,11 @@
 """Unit tests for events.slash_commands/slash_commands.py"""
 
 from unittest.mock import patch, MagicMock
-import pytest
 
 from helper.data import User
 from events.slash_commands import (
-    # run_global_reminder,
-    # run_personal_reminder,
-    # weekly_reminder,
-    send_reminders,
     slash_prs,
+    slack_mrs,
     slash_find_host,
 )
 
@@ -110,4 +106,86 @@ def test_slash_find_host(mock_os):
     mock_os.environ.get.assert_called_once_with("HOST_IP")
     mock_respond.assert_called_once_with(
         f"The host IP of this node is: {mock_os.environ.get.return_value}"
+    )
+
+
+@patch("events.slash_commands.filter_by")
+@patch("events.slash_commands.FindPRsGitLab")
+@patch("events.slash_commands.send_reminders")
+@patch("events.slash_commands.get_config")
+@patch("events.slash_commands.get_token")
+def test_slash_mrs_mine(mock_get_token, mock_get_config, mock_send_reminders, mock_find_mrs, mock_filter_by):
+    """Test the function works when users choose "mine" """
+    mock_get_config.side_effect = [[MOCK_USER], ["group&2Fproject"]]
+    mock_filter_by.return_value = [MOCK_USER]
+    mock_respond = MagicMock()
+    mock_command = {"user_id": MOCK_USER.slack_id, "text": "mine"}
+    mock_ack = MagicMock()
+    slash_mrs(mock_ack, mock_respond, mock_command)
+    mock_ack.assert_called_once_with()
+    mock_get_config.assert_any_call("users")
+    mock_get_config.assert_any_call("projects")
+    mock_get_token.assert_called_once_with("GITLAB_TOKEN")
+    mock_respond.assert_any_call("Finding the MRs...")
+    mock_filter_by.assert_any_call([MOCK_USER], "slack_id", MOCK_USER.slack_id)
+    mock_find_mrs.return_value.run.assert_called_once_with(repos=["group&2Fproject"], token=mock_get_token.return_value)
+    mock_filter_by.assert_any_call(
+        obj_list=mock_find_mrs.return_value.run.return_value,
+        prop="author",
+        value=MOCK_USER.gitlab_name
+    )
+    mock_send_reminders.assert_called_once_with(MOCK_USER.slack_id, mock_filter_by.return_value, True)
+
+
+@patch("events.slash_commands.filter_by")
+@patch("events.slash_commands.FindPRsGitLab")
+@patch("events.slash_commands.send_reminders")
+@patch("events.slash_commands.get_config")
+@patch("events.slash_commands.get_token")
+def test_slash_mrs_all(mock_get_token, mock_get_config, mock_send_reminders, mock_find_mrs, mock_filter_by):
+    """Test the function works when users choose "all" """
+    mock_get_config.side_effect = [[MOCK_USER], ["group&2Fproject"]]
+    mock_filter_by.return_value = [MOCK_USER]
+    mock_respond = MagicMock()
+    mock_command = {"user_id": MOCK_USER.slack_id, "text": "mine"}
+    mock_ack = MagicMock()
+    slash_mrs(mock_ack, mock_respond, mock_command)
+    mock_ack.assert_called_once_with()
+    mock_get_config.assert_any_call("users")
+    mock_get_config.assert_any_call("projects")
+    mock_get_token.assert_called_once_with("GITLAB_TOKEN")
+    mock_respond.assert_any_call("Finding the MRs...")
+    mock_filter_by.assert_any_call([MOCK_USER], "slack_id", MOCK_USER.slack_id)
+    mock_find_mrs.return_value.run.assert_called_once_with(repos=["group&2Fproject"], token=mock_get_token.return_value)
+    mock_send_reminders.assert_called_once_with(MOCK_USER.slack_id, mock_filter_by.return_value, True)
+
+
+@patch("events.slash_commands.get_config")
+def test_slash_mrs_fail(mock_get_config):
+    """Test the function fails if no option is given"""
+    mock_get_config.return_value = [MOCK_USER]
+    mock_respond = MagicMock()
+    mock_command = {"user_id": "mock_slack", "text": ""}
+    mock_ack = MagicMock()
+    slash_prs(mock_ack, mock_respond, mock_command)
+    mock_ack.assert_called_once_with()
+    mock_get_config.assert_called_once_with("users")
+    mock_respond.assert_any_call(
+        "Please provide the correct argument: 'mine' or 'all'."
+    )
+
+
+@patch("events.slash_commands.get_config")
+def test_slash_mrs_no_user(mock_get_config):
+    """Test the function fails if the user is not found in the config file"""
+    mock_get_config.return_value = [MOCK_USER]
+    mock_respond = MagicMock()
+    mock_command = {"user_id": "non_existent_user", "text": ""}
+    mock_ack = MagicMock()
+    slash_mrs(mock_ack, mock_respond, mock_command)
+    mock_ack.assert_called_once_with()
+    mock_get_config.assert_called_once_with("users")
+    mock_respond.assert_any_call(
+        "Could not find your Slack ID non_existent_user in the user map. "
+        "Please contact the service maintainer to fix this."
     )
