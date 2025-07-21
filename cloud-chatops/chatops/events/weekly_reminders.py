8dd177044725525a2a5419bd6d15f2f3d51ccf43
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-07-21 13:24:08 +0100

    MAINT: Update weekly_reminder.py
    Update weekly_reminders.py to use the new config mechanism

--- cloud-chatops/chatops/events/weekly_reminders.py
+++ cloud-chatops/chatops/events/weekly_reminders.py
@@ -3,7 +3,7 @@
 from typing import List, Dict
 from slack_sdk import WebClient
 from helper.data import User, sort_by, filter_by
-from helper.config import get_config, get_token
+from helper.config import get_config, get_secrets
 from notify.slack import PRReminder
 from find_pr.github import GitHub as FindPRsGitHub
 from find_pr.gitlab import GitLab as FindPRsGitLab
@@ -14,18 +14,20 @@ def run_global_reminder(channel: str) -> None:
     This event sends a message to the specified channel with all open PRs.
     :param channel: Channel ID to send the messages to.
     """
+    config = get_config()
+    secrets = get_secrets()
     unsorted_prs = []
-    if get_config("github")["enabled"]:
+    if config.github.enabled:
         unsorted_prs += FindPRsGitHub().run(
-            repos=get_config("repos"), token=get_token("GITHUB_TOKEN")
+            repos=config.github.repositories, token=secrets.GITHUB_TOKEN
         )
-    if get_config("gitlab")["enabled"]:
+    if config.gitlab.enabled:
         unsorted_prs += FindPRsGitLab().run(
-            projects=get_config("projects"), token=get_token("GITLAB_TOKEN")
+            projects=config.gitlab.projects, token=secrets.GITLAB_TOKEN
         )
 
     prs = sort_by(unsorted_prs, "created_at", False)
-    PRReminder(WebClient(token=get_token("SLACK_BOT_TOKEN"))).run(
+    PRReminder(WebClient(token=secrets.SLACK_BOT_TOKEN)).run(
         prs=prs,
         channel=channel,
     )
@@ -37,18 +39,20 @@ def run_personal_reminder(users: List[User], message_no_prs: bool = False) -> No
     :param message_no_prs: Send a message saying there are no PRs open.
     :param users: Users to send reminders to.
     """
+    config = get_config()
+    secrets = get_secrets()
     unsorted_prs = []
-    if get_config("github")["enabled"]:
+    if config.github.enabled:
         unsorted_prs += FindPRsGitHub().run(
-            repos=get_config("repos"), token=get_token("GITHUB_TOKEN")
+            repos=config.github.repositories, token=secrets.GITHUB_TOKEN
         )
-    if get_config("gitlab")["enabled"]:
+    if config.gitlab.enabled:
         unsorted_prs += FindPRsGitLab().run(
-            projects=get_config("projects"), token=get_token("GITLAB_TOKEN")
+            projects=config.gitlab.projects, token=secrets.GITLAB_TOKEN
         )
 
     prs = sort_by(unsorted_prs, "created_at", False)
-    client = WebClient(token=get_token("SLACK_BOT_TOKEN"))
+    client = WebClient(token=secrets.SLACK_BOT_TOKEN)
     for user in users:
         filtered_prs = filter_by(prs, "author", [user.github_name, user.gitlab_name])
         PRReminder(client).run(
@@ -63,6 +67,7 @@ def weekly_reminder(message_data: Dict) -> None:
     This function chooses which type of reminder to call based on its arguments.
     :param message_data: Data from the POST request.
     """
+    config = get_config()
     reminder_type = message_data["reminder_type"]
     channel = message_data.get("channel", "")
     if reminder_type == "global":
@@ -72,7 +77,7 @@ def weekly_reminder(message_data: Dict) -> None:
             )
         run_global_reminder(channel)
     elif reminder_type == "personal":
-        run_personal_reminder(users=get_config("users"), message_no_prs=False)
+        run_personal_reminder(users=config.users, message_no_prs=False)
     else:
         raise ValueError(
             f'Reminder type {reminder_type} is not supported. Use either "global" or "personal".'
--- cloud-chatops/tests/test_weekly_reminders.py
+++ cloud-chatops/tests/test_weekly_reminders.py
@@ -17,14 +17,13 @@ MOCK_USER = User(
     gitlab_name="mock_gitlab",
 )
 
+
 # Disable for patching
 # pylint: disable=R0917
 # pylint: disable=R0913
-
-
 @patch("events.weekly_reminders.sort_by")
 @patch("events.weekly_reminders.WebClient")
-@patch("events.weekly_reminders.get_token")
+@patch("events.weekly_reminders.get_secrets")
 @patch("events.weekly_reminders.get_config")
 @patch("events.weekly_reminders.FindPRsGitLab")
 @patch("events.weekly_reminders.FindPRsGitHub")
@@ -34,52 +33,41 @@ def test_run_global_reminder(
     mock_find_prs_gh,
     mock_find_prs_gl,
     mock_get_config,
-    mock_get_token,
+    mock_get_secrets,
     mock_web_client,
     mock_sort_by,
 ):
     """Test global reminder event"""
-    mock_get_token.side_effect = ["mock_github", "mock_gitlab", "mock_slack"]
-    mock_get_config.side_effect = [
-        {"enabled": True},
-        ["mock_repos"],
-        {"enabled": True},
-        ["mock_projects"],
-    ]
+    mock_config = mock_get_config.return_value
+    mock_secrets = mock_get_secrets.return_value
+    mock_config.github.enabled = True
+    mock_config.gitlab.enabled = True
     run_global_reminder("mock_channel")
+
     mock_find_prs_gh.return_value.run.assert_called_once_with(
-        repos=["mock_repos"], token="mock_github"
+        repos=mock_config.github.repositories, token=mock_secrets.GITHUB_TOKEN
     )
     mock_find_prs_gl.return_value.run.assert_called_once_with(
-        projects=["mock_projects"], token="mock_gitlab"
+        projects=mock_config.gitlab.projects, token=mock_secrets.GITLAB_TOKEN
     )
     unsorted_prs = []
     unsorted_prs += mock_find_prs_gh.return_value.run.return_value
     unsorted_prs += mock_find_prs_gl.return_value.run.return_value
     mock_sort_by.assert_called_once_with(unsorted_prs, "created_at", False)
-    mock_web_client.assert_called_once_with(token="mock_slack")
-    mock_slack.assert_called_once_with(mock_web_client.return_value)
-    mock_slack.return_value.run.assert_called_once_with(
+    mock_web_client.assert_called_once_with(token=mock_secrets.SLACK_BOT_TOKEN)
+    mock_pr_reminder.assert_called_once_with(mock_web_client.return_value)
+    mock_pr_reminder.return_value.run.assert_called_once_with(
         prs=mock_sort_by.return_value, channel="mock_channel"
     )
-    mock_get_config.assert_any_call("repos")
-    mock_get_config.assert_any_call("projects")
-    mock_get_config.assert_any_call("github")
-    mock_get_config.assert_any_call("gitlab")
-    mock_get_token.assert_any_call("GITHUB_TOKEN")
-    mock_get_token.assert_any_call("GITLAB_TOKEN")
-    mock_get_token.assert_any_call("SLACK_BOT_TOKEN")
 
 
 # Disable for patching
 # pylint: disable=R0917
 # pylint: disable=R0913
-
-
 @patch("events.weekly_reminders.sort_by")
 @patch("events.weekly_reminders.filter_by")
 @patch("events.weekly_reminders.WebClient")
-@patch("events.weekly_reminders.get_token")
+@patch("events.weekly_reminders.get_secrets")
 @patch("events.weekly_reminders.get_config")
 @patch("events.weekly_reminders.FindPRsGitLab")
 @patch("events.weekly_reminders.FindPRsGitHub")
@@ -89,25 +77,23 @@ def test_run_personal_reminder(
     mock_find_prs_gh,
     mock_find_prs_gl,
     mock_get_config,
-    mock_get_token,
+    mock_get_secrets,
     mock_web_client,
     mock_filter_by,
     mock_sort_by,
 ):
     """Test personal reminder event"""
-    mock_get_token.side_effect = ["mock_github", "mock_gitlab", "mock_slack"]
-    mock_get_config.side_effect = [
-        {"enabled": True},
-        ["mock_repos"],
-        {"enabled": True},
-        ["mock_projects"],
-    ]
+    mock_config = mock_get_config.return_value
+    mock_secrets = mock_get_secrets.return_value
+    mock_config.github.enabled = True
+    mock_config.gitlab.enabled = True
+
     run_personal_reminder([MOCK_USER])
     mock_find_prs_gh.return_value.run.assert_called_once_with(
-        repos=["mock_repos"], token="mock_github"
+        repos=mock_config.github.repositories, token=mock_secrets.GITHUB_TOKEN
     )
     mock_find_prs_gl.return_value.run.assert_called_once_with(
-        projects=["mock_projects"], token="mock_gitlab"
+        projects=mock_config.gitlab.projects, token=mock_secrets.GITLAB_TOKEN
     )
     unsorted_prs = []
     unsorted_prs += mock_find_prs_gh.return_value.run.return_value
@@ -118,17 +104,9 @@ def test_run_personal_reminder(
         "author",
         [MOCK_USER.github_name, MOCK_USER.gitlab_name],
     )
-    mock_web_client.assert_called_once_with(token="mock_slack")
-    mock_web_client.assert_called_once_with(token="mock_slack")
-    mock_get_config.assert_any_call("repos")
-    mock_get_config.assert_any_call("projects")
-    mock_get_config.assert_any_call("github")
-    mock_get_config.assert_any_call("gitlab")
-    mock_get_token.assert_any_call("GITHUB_TOKEN")
-    mock_get_token.assert_any_call("GITLAB_TOKEN")
-    mock_get_token.assert_any_call("SLACK_BOT_TOKEN")
-    mock_slack.assert_called_once_with(mock_web_client.return_value)
-    mock_slack.return_value.run.assert_called_once_with(
+    mock_web_client.assert_called_once_with(token=mock_secrets.SLACK_BOT_TOKEN)
+    mock_pr_reminder.assert_called_once_with(mock_web_client.return_value)
+    mock_pr_reminder.return_value.run.assert_called_once_with(
         prs=mock_filter_by.return_value,
         channel=MOCK_USER.slack_id,
         message_no_prs=False,
@@ -140,22 +118,23 @@ def test_run_personal_reminder(
 @patch("events.weekly_reminders.get_config")
 def test_weekly_reminder(mock_get_config, mock_personal, mock_global):
     """Test the correct jobs are run"""
-    mock_get_config.return_value = [MOCK_USER]
+    mock_get_config.return_value.users = [MOCK_USER]
     weekly_reminder({"reminder_type": "global", "channel": "mock_channel"})
     mock_global.assert_called_once_with("mock_channel")
 
     weekly_reminder({"reminder_type": "personal"})
-    mock_get_config.assert_any_call("users")
     mock_personal.assert_called_once_with(users=[MOCK_USER], message_no_prs=False)
 
 
-def test_weekly_reminder_fails_unknown_type():
+@patch("events.weekly_reminders.get_config")
+def test_weekly_reminder_fails_unknown_type(_):
     """Test the functions raises an error for an incorrect job."""
     with pytest.raises(ValueError):
         weekly_reminder({"reminder_type": "unknown_value"})
 
 
-def test_weekly_reminder_fails_no_channel():
+@patch("events.weekly_reminders.get_config")
+def test_weekly_reminder_fails_no_channel(_):
     """Test the functions raises an error for not having a channel value."""
     with pytest.raises(ValueError):
         weekly_reminder({"reminder_type": "global"})
